955fb2aa6e2b3c4d6ffe6d95c206937a1d4bad93
Charlie <lucharles3@gmail.com>
2021-07-14 21:59:01 -0400

    test

---
+++ components/layout/header/headerDropdown.js
@@ -0,0 +1,15 @@
+
+const HeaderDropdown = ( { 
+    content,
+} ) => {
+
+    return (
+        <section className='header-dropdown-container'>
+            <div className='header-dropdown-wrapper'>
+                Header Dropdown
+            </div>
+        </section>
+    );
+}
+
+export default HeaderDropdown;
